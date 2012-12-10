b9bc0177801b95afc9d968828dd2a27987c2a732
Cliff Hammerschmidt <tanglebones@gmail.com>
2012-12-10 12:48:44 -0800

    more control over the dynamic loading of dll's.

--- CH.IoC.Infrastructure/IResolver.cs
+++ CH.IoC.Infrastructure/IResolver.cs
@@ -8,5 +8,10 @@ namespace CH.IoC.Infrastructure
         T Resolve<T>() where T:class;
         T[] ResolveAll<T>() where T : class;
         IEnumerable<Tuple<string, IEnumerable<string>>> Registered();
+
+        void LoadDynamicAssemblies(
+            IEnumerable<string> includePrefixes,
+            IEnumerable<string> excludePrefixes,
+            IEnumerable<string> directories);
     }
 }
